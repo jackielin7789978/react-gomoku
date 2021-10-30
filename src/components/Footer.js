2f59ec533f2878e29da602bfcb7b9082283b1c7a
jackie <jackielin7789978@gmail.com>
2021-10-30 15:40:56 +0800

    add GitHub icon and a link to repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const FooterContainer = styled.div`
   position: absolute;
@@ -12,22 +14,34 @@ const FooterContainer = styled.div`
   a {
     color: #8398a3;
     transition: ease 0.2s;
+    margin: 0 4px;
     &:hover {
       color: #41548b;
     }
+    cursor: pointer;
+  }
+  a:last-child {
+    margin: 0;
   }
 `
 
 export default function Footer() {
   return (
     <FooterContainer>
-      © Copyright 2021. Made by {'  '}
+      © Copyright 2021. Made by
       <a
         href='https://github.com/jackielin7789978'
         target='_blank'
         rel='noreferrer'
       >
         Jackie Lin
+      </a>
+      <a
+        href='https://github.com/jackielin7789978/react-gomoku'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <FontAwesomeIcon icon={faGithub} />
       </a>
     </FooterContainer>
   )
